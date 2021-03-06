ff12a22102f6720ba9256336ea481671395a39dc
zhaozhifeng <616089013@qq.com>
2021-03-06 22:20:37 +0800

    demo-logback

--- demo-logback/src/test/java/com/zzf/logback/DemoLogbackApplicationTests.java
+++ demo-logback/src/test/java/com/zzf/logback/DemoLogbackApplicationTests.java
@@ -1,13 +1,19 @@
 package com.zzf.logback;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.slf4j.MDC;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class DemoLogbackApplicationTests {
 
+
 	@Test
 	void contextLoads() {
+		MDC.put("id","1");
+		log.info("info 日志");
 	}
 
 }
