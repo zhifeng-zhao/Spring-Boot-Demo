f2f13f61174560136e5a9a05e97043dd5bb80afa
zhaozhifeng <616089013@qq.com>
2021-03-13 01:05:23 +0800

    feat: demo-beetl

--- demo-beetl/src/main/java/com/zzf/demobeetl/controller/DemoController.java
+++ demo-beetl/src/main/java/com/zzf/demobeetl/controller/DemoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -28,6 +29,7 @@ public class DemoController {
             userList.add(new User(nameList[i]));
         }
         modelAndView.addObject("userList",userList);
+        modelAndView.addObject("date",new Date());
         return modelAndView;
     }
 }
